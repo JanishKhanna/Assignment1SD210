efa1cc84758955e1dc444f79d9c4716bbb286710
Janish Khanna <janish.khanna233@gmail.com>
2019-04-26 09:40:01 -0500

    Add project files.

---
+++ Assignment1SD210/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment1SD210.MvcApplication" Language="C#" %>
---
+++ Assignment1SD210/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment1SD210.Startup))]
+namespace Assignment1SD210
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
